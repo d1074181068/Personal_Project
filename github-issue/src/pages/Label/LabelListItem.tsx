176333b736b134d1f7e52343774556d2e8ce6949
Xiang <wiwi19991214@gmail.com>
2022-09-21 16:19:03 +0800

    LabelList Mobile layout complete

--- github-issue/src/pages/Label/LabelListItem.tsx
+++ github-issue/src/pages/Label/LabelListItem.tsx
@@ -1,7 +1,7 @@
 //Libraries
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-
+import { KebabHorizontalIcon } from '@primer/octicons-react'
 //components
 import LabelItem from './LabelItem'
 
@@ -10,6 +10,13 @@ type PropsType = {
   labelDesc: string
   useLabelIssueQty: number
   colorCode: string
+}
+type MoreActionBtnType = {
+  bgColor: string
+  fillColor: string
+}
+type MoreActionWrapperPropsType = {
+  display: string
 }
 
 const Item = styled.li`
@@ -26,13 +33,86 @@ const Item = styled.li`
 const LabelDesc = styled.span`
   color: rgb(87, 96, 106);
   font-size: 12px;
+  @media (max-width: 767px) {
+    display: none;
+  }
 `
-const ActionBtnWrapper = styled.div``
+const LabelUserStatus = styled.a`
+  color: rgb(87, 96, 106);
+  font-size: 12px;
+  :hover {
+    color: #0969da;
+    text-decoration: underline;
+  }
+  @media (max-width: 767px) {
+    display: none;
+  }
+`
+
+const ActionBtnWrapper = styled.div`
+  display: block;
+  @media (max-width: 1011px) {
+    display: none;
+  }
+`
 
 const ActionBtn = styled.a`
   margin-right: 20px;
   font-size: 12px;
   color: rgb(87, 96, 106);
+`
+
+const MobileMoreBtnWrapper = styled.div`
+  position: relative;
+  display: none;
+  @media (max-width: 1011px) {
+    display: block;
+  }
+`
+const MobileActionBtnIcon = styled(KebabHorizontalIcon)``
+
+const MobileMoreActionBtn = styled.button<MoreActionBtnType>`
+  display: block;
+  padding: 6px 12px;
+  border: 1px solid rgb(208, 215, 222);
+  border-radius: 6px;
+  margin-right: 20px;
+  cursor: pointer;
+  background-color: ${(props) => props.bgColor};
+  & > ${MobileActionBtnIcon} {
+    fill: ${(props) => props.fillColor};
+  }
+  :hover {
+    background-color: #0969da;
+    & > ${MobileActionBtnIcon} {
+      fill: white;
+    }
+  }
+`
+const MobileActionBtnWrapper = styled.div<MoreActionWrapperPropsType>`
+  position: absolute;
+  top: 35px;
+  right: 20px;
+  z-index: 10;
+  background-color: white;
+  border: 1px solid rgb(208, 215, 222);
+  border-radius: 6px;
+  width: 158px;
+  display: ${(props) => props.display};
+`
+const MobileActionBtn = styled.a`
+  padding: 10px;
+  display: block;
+  :hover {
+    background-color: #0969da;
+    color: white;
+  }
+  :first-child {
+    border-radius: 6px 6px 0px 0px;
+  }
+  :last-child {
+    border-radius: 0px 0px 6px 6px;
+  }
 `
 
 function LabelListItem({
@@ -41,15 +121,34 @@ function LabelListItem({
   useLabelIssueQty,
   colorCode
 }: PropsType) {
+  const [moreActionBtnActive, setMoreActionBtnActive] = useState(false)
   return (
     <Item>
       <LabelItem labelName={labelName} colorCode={colorCode} />
       <LabelDesc>{labelDesc}</LabelDesc>
-      <LabelDesc>{useLabelIssueQty} open issue or pull request</LabelDesc>
+      <LabelUserStatus>
+        {useLabelIssueQty} open issue or pull request
+      </LabelUserStatus>
       <ActionBtnWrapper>
         <ActionBtn>Edit</ActionBtn>
         <ActionBtn>Delete</ActionBtn>
       </ActionBtnWrapper>
+      <MobileMoreBtnWrapper>
+        <MobileMoreActionBtn
+          onClick={() => setMoreActionBtnActive((prev) => !prev)}
+          onBlur={() => setMoreActionBtnActive(false)}
+          bgColor={moreActionBtnActive ? '#0969da' : 'white'}
+          fillColor={moreActionBtnActive ? 'white' : 'black'}
+        >
+          <MobileActionBtnIcon />
+        </MobileMoreActionBtn>
+        <MobileActionBtnWrapper
+          display={moreActionBtnActive ? 'block' : 'none'}
+        >
+          <MobileActionBtn>Edit</MobileActionBtn>
+          <MobileActionBtn>Delete</MobileActionBtn>
+        </MobileActionBtnWrapper>
+      </MobileMoreBtnWrapper>
     </Item>
   )
 }
