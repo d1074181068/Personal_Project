168609578d662f9680aae05d9e7965b316a8ce0e
Xiang <wiwi19991214@gmail.com>
2022-09-25 17:11:05 +0800

    create labelListItem storybook

---
+++ github-issue/src/pages/Label/LabelListItem.stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Meta, Story } from '@storybook/react'
+import LabelListItem from './LabelListItem'
+import { Provider } from 'react-redux'
+import { store } from '../../redux/store'
+type StorybookType = {
+  labelName: string
+  labelDesc: string
+  useLabelIssueQty: number
+  colorCode: string
+}
+
+export default {
+  title: 'Example/page_label_management/LabelListItem',
+  component: LabelListItem,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen'
+  }
+} as Meta
+
+const Template: Story<StorybookType> = (args: StorybookType) => (
+  <Provider store={store}>
+    <ul
+      style={{
+        margin: '20px',
+        padding: '0px',
+        listStyle: 'none'
+      }}
+    >
+      <LabelListItem {...args} />
+    </ul>
+  </Provider>
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  labelName: 'Frank',
+  labelDesc: 'Frank is coming',
+  useLabelIssueQty: 2,
+  colorCode: '#FF0000'
+}
